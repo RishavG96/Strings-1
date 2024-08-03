b149512e69cc025795a7826420161f58ee5c6838
Rishav Gupta <gupta.rishav2608@gmail.com>
2024-08-03 15:25:17 +0530

    Create CustomSortString.java

---
+++ CustomSortString.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String customSortString(String order, String s) {
+        if(s == null || s.length() == 0) {
+            return s;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for(int i = 0; i < order.length(); i++) {
+            char c = order.charAt(i);
+            if(map.containsKey(c)) {
+                int times = map.get(c);
+                for(int j = 0; j < times; j++) {
+                    sb.append(c);
+                }
+                map.remove(c);
+            }
+        }
+        for(char key: map.keySet()) {
+            int times = map.get(key);
+            for(int j = 0; j < times; j++) {
+                sb.append(key);
+            }
+        }
+        return sb.toString();
+    }
+}
